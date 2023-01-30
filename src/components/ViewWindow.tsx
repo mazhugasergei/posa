8912e035473f303ecd9b9891dbfc0124cb8b89dc
Markus Wedler <markuswedler8@gmail.com>
2023-01-31 01:23:58 +1000

    .

--- src/components/ViewWindow.tsx
+++ src/components/ViewWindow.tsx
@@ -1,15 +1,26 @@
 // react
 import { useEffect } from "react"
 // images
-import download from "../images/ui/download.svg"
+import downloadBtn from "../images/ui/download.svg"
 
 const ViewWindow = ({ url, close }: { url: string, close: ()=>void }) => {
+  const downloadImage = async (url: string) => {
+    await fetch(url)
+      .then(res => {return res.blob()})
+      .then(blob =>{
+        const link = document.createElement("a")
+        link.href = window.URL.createObjectURL(blob)
+        link.download = ""
+        link.click()
+      })
+  }
+
   return (
     <div className="ViewWindow">
       <div className="close" onClick={close} />
       <nav className="control">
         <label className="close" onClick={close} />
-        <label className="download"><img src={download} alt="" /></label>
+        <label className="download" onClick={()=>{downloadImage(url)}}><img src={downloadBtn} alt="" /></label>
       </nav>
       <div className="container">
         <img className="img" src={url} alt="" />
