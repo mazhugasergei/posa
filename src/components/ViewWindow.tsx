fb71f27028cbbf60f4ba81c2764c099a489d98a9
Markus Wedler <markuswedler8@gmail.com>
2023-01-30 18:42:20 +1000

    .

--- src/components/ViewWindow.tsx
+++ src/components/ViewWindow.tsx
@@ -1,9 +1,19 @@
+// react
+import { useEffect } from "react"
+
 const ViewWindow = ({ url, close }: { url: string, close: ()=>void }) => {
+  useEffect(()=>{
+    // close on outside click
+    document.querySelector(".ViewWindow")?.addEventListener("click", (e)=>{
+      if(e.target !== document.querySelector(".ViewWindow img")) close()
+    })
+  }, [])
+
   return (
     <div className="ViewWindow">
       <div className="container">
         <img src={url} alt="" />
-        <button className="btn light" onClick={close}>Close</button>
+        <button className="btn light">Close</button>
       </div>
     </div>
   )
